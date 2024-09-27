00bbd628314619f05dc21d4766afffcf294ad775
YigitAgr <yigit.agar22@gmail.com>
2024-09-27 15:03:20 +0300

    Dto's added.

---
+++ demo/src/main/java/com/challenge/demo/Dto/ProductDto.java
@@ -0,0 +1,13 @@
+package com.challenge.demo.Dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+    private int id;
+    private String name;
+    private int price;
+    private int stock;
+
+}
